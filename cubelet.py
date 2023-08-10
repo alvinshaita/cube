02918efc717beb9c231bf46e32cb1d4920710e2d
alvinshaita <alvinshaita@gmail.com>
2023-08-10 18:28:15 +0300

    clean cubelet

--- cubelet.py
+++ cubelet.py
@@ -1,13 +1,5 @@
 import numpy as np
 
-U_SLICE = (slice(None), 0, slice(None))
-D_SLICE = (slice(None), -1, slice(None))
-L_SLICE = (slice(None), slice(None), 0)
-R_SLICE = (slice(None), slice(None), -1)
-F_SLICE = (0, slice(None), slice(None))
-B_SLICE = (-1, slice(None), slice(None))
-
-
 class Cubelet:
 	def __init__(self, pos):
 		self.pos = pos
