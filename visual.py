b5586842845b6aa87035602735e6e0ea96d48dc3
alvinshaita <alvinshaita@gmail.com>
2025-01-22 05:00:51 +0300

    gui from state

--- visual.py
+++ visual.py
@@ -17,23 +17,36 @@ BLUE = (0, 0, 255)
 GREEN = (0, 255, 0)
 BLACK = (0, 0, 0)
 
-# Cube initialization: 6 faces with 3x3 grids
-cube =  {
-	"U": [[YELLOW] * 3 for _ in range(3)],  # Up
-	"D": [[WHITE] * 3 for _ in range(3)], # Down
-	"F": [[RED] * 3 for _ in range(3)],    # Front
-	"B": [[ORANGE] * 3 for _ in range(3)], # Back
-	"L": [[BLUE] * 3 for _ in range(3)],   # Left
-	"R": [[GREEN] * 3 for _ in range(3)],  # Right
-}
+# state = "yyyyyyyyybbbbbbbbbrrrrrrrrrgggggggggooooooooowwwwwwwww"
+state = "yyryyryyrbbbbbbbbbrrwrrwrrwgggggggggyooyooyoowwowwowwo"
 
-# Draw a face of the cube
-def draw_face(screen, face, x, y):
+
+colors = {"y": YELLOW, "b": BLUE, "r": RED, "g": GREEN ,"o": ORANGE, "w": WHITE}
+
+def draw_from_state(screen):
+	u = 50
+	l = 100
+	# s = 200
+	s = CUBE_SIZE
+
+	# # Draw the cube faces (simple flat layout)
+	draw_state_face(screen, state[:9], l+s, u)
+	draw_state_face(screen, state[9:18], l, u+s)
+	draw_state_face(screen, state[18:27], l+s, u+s)
+	draw_state_face(screen, state[27:36], l+(2*s), u+s)
+	draw_state_face(screen, state[36:45], l+(3*s), u+s)
+	draw_state_face(screen, state[45:54], l+s, u+(2*s))
+
+
+def draw_state_face(screen, face_state, x, y):
+	face = [[colors[i] for i in face_state[j*3:(j*3)+3]] for j in range(3)]
+
 	for i, row in enumerate(face):
 		for j, color in enumerate(row):
 			rect = pygame.Rect(x + j * SQUARE_SIZE, y + i * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE)
 			pygame.draw.rect(screen, color, rect)
 			pygame.draw.rect(screen, BLACK, rect, 1)  # Border
+
 
 # Main loop
 def main():
@@ -51,17 +64,8 @@ def main():
 			if event.type == pygame.QUIT:
 				running = False
 
-		u = 50
-		l = 100
-		# s = 200
-		s = CUBE_SIZE
-		# Draw the cube faces (simple flat layout)
-		draw_face(screen, cube["U"], l+s, u)  # Up face
-		draw_face(screen, cube["F"], l+s, u+s) # Front face
-		draw_face(screen, cube["D"], l+s, u+(2*s)) # Down face
-		draw_face(screen, cube["L"], l, u+s) # Left face
-		draw_face(screen, cube["R"], l+(2*s), u+s) # Right face
-		draw_face(screen, cube["B"], l+(3*s), u+s) # Back face
+
+		draw_from_state(screen)
 
 		pygame.display.flip()
 		clock.tick(FPS)
