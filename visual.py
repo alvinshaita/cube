c74d4d4ca6e9dc161cee4ed9af23c79d4dab1824
alvinshaita <alvinshaita@gmail.com>
2025-12-02 18:41:55 +0300

    visualization init

--- visual.py
+++ visual.py
@@ -29,6 +29,8 @@ state = "yyryyryyrbbbbbbbbbrrwrrwrrwgggggggggyooyooyoowwowwowwo"
 
 # colors = {"y": YELLOW, "b": BLUE, "r": RED, "g": GREEN ,"o": ORANGE, "w": WHITE}
 
+bbb = [None, None]
+rects = []
 
 
 from cube import Cube
@@ -38,7 +40,47 @@ cube = Cube(CUBE_SIZE)
 # cube = attridict(state=state)
 
 
-def draw_from_state(screen):
+
+
+def init_draw_from_state(screen):
+	for i in range(CUBE_SIZE):
+		aaa = cube.state[i*CUBE_SIZE:(i+1)*CUBE_SIZE]
+		for j, a in enumerate(aaa):
+			color = colors[a]
+			rect = pygame.Rect(left_margin + FACE_SIZE + j * SQUARE_SIZE, top_margin + i * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE)
+			rects.append([rect, color])
+
+			pygame.draw.rect(screen, color, rect)
+			pygame.draw.rect(screen, colors.black, rect, 1)
+
+
+	for i in range(CUBE_SIZE):
+		for f in range(4):
+			aaa = cube.state[(CUBE_SIZE+i+CUBE_SIZE*f)*CUBE_SIZE:(CUBE_SIZE+i+CUBE_SIZE*f+1)*CUBE_SIZE]
+			for j, a in enumerate(aaa):
+				color = colors[a]
+				rect = pygame.Rect(left_margin + (f*FACE_SIZE) + j * SQUARE_SIZE, top_margin + FACE_SIZE + i * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE)
+				rects.append([rect, color])
+
+				pygame.draw.rect(screen, color, rect)
+				pygame.draw.rect(screen, colors.black, rect, 1)
+
+
+	for i in range(CUBE_SIZE):
+		aaa = cube.state[(i+(CUBE_SIZE*5))*CUBE_SIZE:(i+(CUBE_SIZE*5)+1)*CUBE_SIZE]
+		for j, a in enumerate(aaa):
+			color = colors[a]
+			rect = pygame.Rect(left_margin + FACE_SIZE + j * SQUARE_SIZE, top_margin + (2*FACE_SIZE) + i * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE)
+			rects.append([rect, color])
+
+			pygame.draw.rect(screen, color, rect)
+			pygame.draw.rect(screen, colors.black, rect, 1)
+
+
+
+
+
+def move_tiles(screen):
 	for i in range(CUBE_SIZE):
 		aaa = cube.state[i*CUBE_SIZE:(i+1)*CUBE_SIZE]
 		for j, a in enumerate(aaa):
@@ -67,28 +109,81 @@ def draw_from_state(screen):
 			pygame.draw.rect(screen, colors.black, rect, 1)
 
 
+
+
+def draw_from_state(screen):
+	for i in range(CUBE_SIZE):
+		aaa = cube.state[i*CUBE_SIZE:(i+1)*CUBE_SIZE]
+		for j, a in enumerate(aaa):
+			color = colors[a]
+			rect = pygame.Rect(left_margin + FACE_SIZE + j * SQUARE_SIZE, top_margin + i * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE)
+			# pygame.draw.rect(screen, color, rect)
+			# pygame.draw.rect(screen, colors.black, rect, 1)
+
+
+	for i in range(CUBE_SIZE):
+		for f in range(4):
+			aaa = cube.state[(CUBE_SIZE+i+CUBE_SIZE*f)*CUBE_SIZE:(CUBE_SIZE+i+CUBE_SIZE*f+1)*CUBE_SIZE]
+			for j, a in enumerate(aaa):
+				color = colors[a]
+				rect = pygame.Rect(left_margin + (f*FACE_SIZE) + j * SQUARE_SIZE, top_margin + FACE_SIZE + i * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE)
+				pygame.draw.rect(screen, color, rect)
+				pygame.draw.rect(screen, colors.black, rect, 1)
+
+
+	for i in range(CUBE_SIZE):
+		aaa = cube.state[(i+(CUBE_SIZE*5))*CUBE_SIZE:(i+(CUBE_SIZE*5)+1)*CUBE_SIZE]
+		for j, a in enumerate(aaa):
+			color = colors[a]
+			rect = pygame.Rect(left_margin + FACE_SIZE + j * SQUARE_SIZE, top_margin + (2*FACE_SIZE) + i * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE)
+			pygame.draw.rect(screen, color, rect)
+			pygame.draw.rect(screen, colors.black, rect, 1)
+
+
 # Main loop
 def main():
 	pygame.init()
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 	pygame.display.set_caption("Rubik's Cube")
 	# clock = pygame.time.Clock()
+
+	screen.fill(colors.black)
+	init_draw_from_state(screen)
 	
 	running = True
 	while running:
-		screen.fill(colors.black)
+		# screen.fill(colors.black)
 		
 		# Handle events
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				running = False
 			elif event.type == pygame.MOUSEBUTTONDOWN:
+				# draw_from_state(screen)
+
 				print("click")
-				mouse_pos = event.pos
-				cube.rotate_r(index_to_move=0)
+				print(rects)
+				print(bbb[0])
+				# bbb[0].move(20, 0)
+				# bbb[0].move_ip(20, 0)
+				# pygame.draw.rect(screen, bbb[1], bbb[0])
+				# print(event)
+				# print(dir(event))
+				# print(event.button)
+				# print(event.dict)
+				# print(event.type)
+				# print(event.window)
+				# mouse_pos = event.pos
+				# cube.rotate_r(index_to_move=0)
+				
+
+				# draw_from_state(screen)
+				# pygame.display.flip()
+
+			# print(event)
 
 
-		draw_from_state(screen)
+				# draw_from_state(screen)
 
 		pygame.display.flip()
 		# clock.tick(FPS)
